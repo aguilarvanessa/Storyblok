307e20f0b2e3086fa6a61d35ad5f0259b397f8b5
aguilarvanessa <106997664+aguilarvanessa@users.noreply.github.com>
2022-11-03 07:52:01 -0500

    Update index.js
    Removed Token

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {storyblokInit, apiPlugin} from "@storyblok/react"
 storyblokInit({
-  accessToken: 'kb2JDdBc58v8pho0HK3Utgtt',
+  accessToken: '',
   use:[apiPlugin],
   version: 'published'
   
